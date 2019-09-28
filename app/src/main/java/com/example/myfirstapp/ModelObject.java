7ede63239d38f9a13068daeaa6d474fd1fa5408f
MBatroukh <mbatroukh@gmail.com>
2019-09-27 23:36:05 -0400

    Renaming some files and got slider to work

--- app/src/main/java/com/example/myfirstapp/ModelObject.java
+++ app/src/main/java/com/example/myfirstapp/ModelObject.java
@@ -1,4 +1,25 @@
 package com.example.myfirstapp;
 
-public class ModelObject {
+public enum ModelObject {
+
+    VISUALIZER(R.string.visualizer, R.layout.fragment_visualizer),
+    ARABIC(R.string.arabic, R.layout.fragment_arabic),
+    TRANSLITERATION(R.string.transliteration, R.layout.fragment_transliteration);
+
+    private int mTitleResId;
+    private int mLayoutResId;
+
+    ModelObject(int titleResId, int layoutResId) {
+        mTitleResId = titleResId;
+        mLayoutResId = layoutResId;
+    }
+
+    public int getTitleResId() {
+        return mTitleResId;
+    }
+
+    public int getLayoutResId() {
+        return mLayoutResId;
+    }
+
 }
